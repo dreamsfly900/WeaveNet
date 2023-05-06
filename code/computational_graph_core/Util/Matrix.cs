5aeb8b8317485ff1a925f21ffbdb413bd53dbc39
葛杰 <xingyu900@live.com>
2023-05-06 12:05:01 +0800

    增加矩阵计算

--- code/computational_graph_core/Util/Matrix.cs
+++ code/computational_graph_core/Util/Matrix.cs
@@ -2925,5 +2925,228 @@ namespace FCN
             }
             return result;
         }
+
+        /// <summary>
+        /// 矩阵转置
+        /// </summary>
+        /// <param name="Ma"></param>
+        /// <param name="Mb"></param>
+        /// <returns></returns>
+        public static Matrix MatrixTrans(Matrix Ma)
+        {
+            int m = Ma.values.GetLength(0);
+            int n = Ma.values.GetLength(1);
+            Matrix Mc = new Matrix(n, m);
+            float[,] c = Mc.values;
+            float[,] a = Ma.values;
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                    c[i, j] = a[j, i];
+
+            return Mc;
+        }
+        /// <summary>
+        /// 矩阵求逆(高斯法)
+        /// </summary>
+        /// <param name="Ma"></param>
+        /// <returns></returns>
+        public static Matrix MatrixInv(Matrix Ma)
+        {
+            int m = Ma.values.GetLength(0);
+            int n = Ma.values.GetLength(1);
+            if (m != n)
+            {
+                Exception myException = new Exception("数组维数不匹配");
+                throw myException;
+            }
+            Matrix Mc = new Matrix(m, n);
+            float[,] a0 = Ma.values;
+            float[,] a = (float[,])a0.Clone();
+            float[,] b = Mc.values;
+
+            int i, j, row, k;
+            float max, temp;
+
+            //单位矩阵
+            for (i = 0; i < n; i++)
+            {
+                b[i, i] = 1;
+            }
+            for (k = 0; k < n; k++)
+            {
+                max = 0; row = k;
+                //找最大元，其所在行为row
+                for (i = k; i < n; i++)
+                {
+                    temp = Math.Abs(a[i, k]);
+                    if (max < temp)
+                    {
+                        max = temp;
+                        row = i;
+                    }
+
+                }
+                if (max == 0)
+                {
+                    Exception myException = new Exception("没有逆矩阵");
+                    throw myException;
+                }
+                //交换k与row行
+                if (row != k)
+                {
+                    for (j = 0; j < n; j++)
+                    {
+                        temp = a[row, j];
+                        a[row, j] = a[k, j];
+                        a[k, j] = temp;
+
+                        temp = b[row, j];
+                        b[row, j] = b[k, j];
+                        b[k, j] = temp;
+                    }
+
+                }
+
+                //首元化为1
+                for (j = k + 1; j < n; j++) a[k, j] /= a[k, k];
+                for (j = 0; j < n; j++) b[k, j] /= a[k, k];
+
+                a[k, k] = 1;
+
+                //k列化为0
+                //对a
+                for (j = k + 1; j < n; j++)
+                {
+                    for (i = 0; i < k; i++) a[i, j] -= a[i, k] * a[k, j];
+                    for (i = k + 1; i < n; i++) a[i, j] -= a[i, k] * a[k, j];
+                }
+                //对b
+                for (j = 0; j < n; j++)
+                {
+                    for (i = 0; i < k; i++) b[i, j] -= a[i, k] * b[k, j];
+                    for (i = k + 1; i < n; i++) b[i, j] -= a[i, k] * b[k, j];
+                }
+                for (i = 0; i < n; i++) a[i, k] = 0;
+                a[k, k] = 1;
+            }
+
+            return Mc;
+        }
+        /// <summary>
+        /// 矩阵求逆(伴随矩阵法)
+        /// </summary>
+        /// <param name="Ma"></param>
+        /// <returns></returns>
+        public static Matrix MatrixInvByCom(Matrix Ma)
+        {
+            double d = MatrixDet(Ma);
+            if (d == 0)
+            {
+                Exception myException = new Exception("没有逆矩阵");
+                throw myException;
+            }
+            Matrix Ax = MatrixCom(Ma);
+            Matrix An = Matrix.multiply( Ax, (float)(1.0f / d));
+            return An;
+        }
+        /// <summary>
+        /// 对应行列式的代数余子式矩阵
+        /// </summary>
+        /// <param name="Ma"></param>
+        /// <returns></returns>
+        public static Matrix MatrixSpa(Matrix Ma, int ai, int aj)
+        {
+            int m = Ma.values.GetLength(0);
+            int n = Ma.values.GetLength(1);
+            if (m != n)
+            {
+                Exception myException = new Exception("数组维数不匹配");
+                throw myException;
+            }
+            int n2 = n - 1;
+            Matrix Mc = new Matrix(n2, n2);
+            float[,] a = Ma.values;
+            float[,] b = Mc.values;
+
+            //左上
+            for (int i = 0; i < ai; i++)
+                for (int j = 0; j < aj; j++)
+                {
+                    b[i, j] = a[i, j];
+                }
+            //右下
+            for (int i = ai; i < n2; i++)
+                for (int j = aj; j < n2; j++)
+                {
+                    b[i, j] = a[i + 1, j + 1];
+                }
+            //右上
+            for (int i = 0; i < ai; i++)
+                for (int j = aj; j < n2; j++)
+                {
+                    b[i, j] = a[i, j + 1];
+                }
+            //左下
+            for (int i = ai; i < n2; i++)
+                for (int j = 0; j < aj; j++)
+                {
+                    b[i, j] = a[i + 1, j];
+                }
+            //符号位
+            if ((ai + aj) % 2 != 0)
+            {
+                for (int i = 0; i < n2; i++)
+                    b[i, 0] = -b[i, 0];
+
+            }
+            return Mc;
+        }
+        /// <summary>
+        /// 矩阵的行列式
+        /// </summary>
+        /// <param name="Ma"></param>
+        /// <returns></returns>
+        public static float MatrixDet(Matrix Ma)
+        {
+            int m = Ma.values.GetLength(0);
+            int n = Ma.values.GetLength(1);
+            if (m != n)
+            {
+                Exception myException = new Exception("数组维数不匹配");
+                throw myException;
+            }
+            float[,] a = Ma.values;
+            if (n == 1) return a[0, 0];
+
+            float D = 0;
+            for (int i = 0; i < n; i++)
+            {
+                D += a[1, i] * MatrixDet(MatrixSpa(Ma, 1, i));
+            }
+            return D;
+        }
+
+        /// <summary>
+        /// 矩阵的伴随矩阵
+        /// </summary>
+        /// <param name="Ma"></param>
+        /// <returns></returns>
+        public static Matrix MatrixCom(Matrix Ma)
+        {
+            int m = Ma.values.GetLength(0);
+            int n = Ma.values.GetLength(1);
+            Matrix Mc = new Matrix(m, n);
+            float[,] c = Mc.values;
+            float[,] a = Ma.values;
+
+            for (int i = 0; i < m; i++)
+                for (int j = 0; j < n; j++)
+                    c[i, j] = MatrixDet(MatrixSpa(Ma, j, i));
+
+            return Mc;
+        }
+
+ 
     }
 }
